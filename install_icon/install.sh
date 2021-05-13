083d3bf1fc97451bd7d526389ed49869e0525fca
mbridak <michael.bridak@gmail.com>
2021-05-13 12:28:49 -0700

    Icon install for binary

---
+++ install_icon/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+xdg-icon-resource install --size 64 --context apps --mode user k6gte-SOTAcracker.png k6gte-SOTAcracker
+
+xdg-desktop-icon install k6gte-SOTAcracker.desktop
+
+xdg-desktop-menu install k6gte-SOTAcracker.desktop
+
---
+++ install_icon/uninstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+xdg-icon-resource uninstall --size 64 k6gte-SOTAcracker
+
+xdg-desktop-icon uninstall k6gte-SOTAcracker.desktop
+
+xdg-desktop-menu uninstall k6gte-SOTAcracker.desktop
+
