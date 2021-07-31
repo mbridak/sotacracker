0d260f9d5efa28c0d26167b0c024383a51744a44
mbridak <michael.bridak@gmail.com>
2021-07-31 14:10:50 -0700

    Added band/mode filter

--- sotacracker.py
+++ sotacracker.py
@@ -16,6 +16,12 @@ class MainWindow(QtWidgets.QMainWindow):
         super().__init__(parent)
         uic.loadUi(self.relpath("dialog.ui"), self)
         self.listWidget.clicked.connect(self.spotclicked)
+        self.comboBox_band.currentTextChanged.connect(self.getspots)
+        self.comboBox_mode.currentTextChanged.connect(self.getspots)
+        self.bw['LSB'] = '2400'
+        self.bw['USB'] = '2400'
+        self.bw['FM'] = '15000'
+        self.bw['CW'] = '200'
 
     def relpath(self, filename):
         try:
@@ -36,20 +42,22 @@ class MainWindow(QtWidgets.QMainWindow):
         justonce=[]
         count = 0
         for i in spots:
-            i['activatorCallsign']=i['activatorCallsign'].replace("\n", "").upper()
-            i['activatorCallsign']=i['activatorCallsign'].replace(" ", "")
-            if i['activatorCallsign'] in justonce:
-                continue
-            count += 1
-            if count > 20:
-                return
-            justonce.append(i['activatorCallsign'])
-            summit = f"{i['associationCode'].rjust(3)}/{i['summitCode'].rjust(6)}" # {i['summitDetails']}
-            spot = f"{i['timeStamp'][11:16]} {i['activatorCallsign'].rjust(10)} {summit.ljust(9)} {i['frequency'].rjust(8)} {i['mode'].upper()}"
-            self.listWidget.addItem(spot)
-            if spot[5:] == self.lastclicked[5:]:
-                founditem = self.listWidget.findItems(spot[5:], QtCore.Qt.MatchFlag.MatchContains)
-                founditem[0].setSelected(True)
+            if self.comboBox_mode.currentText() == 'All' or i['mode'].upper() == self.comboBox_mode.currentText():
+                if self.comboBox_band.currentText() == 'All' or self.getband(i['frequency'].split('.')[0]) == self.comboBox_band.currentText():
+                    i['activatorCallsign']=i['activatorCallsign'].replace("\n", "").upper()
+                    i['activatorCallsign']=i['activatorCallsign'].replace(" ", "")
+                    if i['activatorCallsign'] in justonce:
+                        continue
+                    count += 1
+                    if count > 20:
+                        return
+                    justonce.append(i['activatorCallsign'])
+                    summit = f"{i['associationCode'].rjust(3)}/{i['summitCode'].rjust(6)}" # {i['summitDetails']}
+                    spot = f"{i['timeStamp'][11:16]} {i['activatorCallsign'].rjust(10)} {summit.ljust(9)} {i['frequency'].rjust(8)} {i['mode'].upper()}"
+                    self.listWidget.addItem(spot)
+                    if spot[5:] == self.lastclicked[5:]:
+                        founditem = self.listWidget.findItems(spot[5:], QtCore.Qt.MatchFlag.MatchContains)
+                        founditem[0].setSelected(True)
 
     def spotclicked(self):
         """
@@ -74,12 +82,42 @@ class MainWindow(QtWidgets.QMainWindow):
                     mode = 'USB'
                 else:
                     mode = 'LSB'
-            command = 'M '+mode+ ' 0\n'
+            command = 'M '+mode+ ' ' + self.bw[mode] + '\n'
             radiosocket.send(command.encode('ascii'))
             radiosocket.shutdown(socket.SHUT_RDWR)
             radiosocket.close()
         except:
             pass 
+    
+    def getband(self, freq):
+        if freq.isnumeric():
+            frequency = int(freq)
+            if frequency == 1 or frequency == 2:
+                return "160"
+            if frequency == 3 or frequency == 4:
+                return "80"
+            if frequency == 5:
+                return "60"
+            if frequency == 7:
+                return "40"
+            if frequency == 10:
+                return "30"
+            if frequency == 14:
+                return "20"
+            if frequency == 18:
+                return "17"
+            if frequency == 21:
+                return "15"
+            if frequency == 24:
+                return "12"
+            if frequency == 28 or frequency == 29:
+                return "10"
+            if frequency >= 50 and frequency <= 54:
+                return "6"
+            if frequency >= 144 and frequency <= 148:
+                return "2"
+        else:
+            return "0"
 
 app = QtWidgets.QApplication(sys.argv)
 app.setStyle('Fusion')
